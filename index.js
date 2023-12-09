ad313b3b60f0a11e971a3efb3924d95d31a369fd
Ajit Mote <123333490+Whiterows42@users.noreply.github.com>
2023-12-09 10:01:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,98 @@
+function show() {
+    var showElement = document.getElementById("show");
+  
+    if (showElement.style.display === "block") {
+      showElement.style.display = "none";
+      let set = document.getElementById("cros")
+      set.setAttribute("class", "fa fa-plus")
+    } else if (showElement.style.display === "none") {
+      showElement.style.display = "block";
+      let set = document.getElementById("cros")
+      set.setAttribute("class", "fa fa-times")
+    } else {
+      showElement.style.display = "none";
+    }
+  }
+   function show2(){
+    var showElement = document.getElementById("show2");
+  
+    if (showElement.style.display === "block") {
+      showElement.style.display = "none";
+      let set = document.getElementById("cros2")
+      set.setAttribute("class", "fa fa-plus")
+    } else if (showElement.style.display === "none") {
+      showElement.style.display = "block";
+      let set = document.getElementById("cros2")
+      set.setAttribute("class", "fa fa-times")
+    } else {
+      showElement.style.display = "none";
+    }
+  }
+   function show3(){
+    var showElement = document.getElementById("show3");
+  
+    if (showElement.style.display === "block") {
+      showElement.style.display = "none";
+      let set = document.getElementById("cros3")
+      set.setAttribute("class", "fa fa-plus")
+    } else if (showElement.style.display === "none") {
+      showElement.style.display = "block";
+      let set = document.getElementById("cros3")
+      set.setAttribute("class", "fa fa-times")
+    } else {
+      showElement.style.display = "none";
+    }
+  }
+   function show4(){
+    var showElement = document.getElementById("show4");
+  
+    if (showElement.style.display === "block") {
+      showElement.style.display = "none";
+      let set = document.getElementById("cros4")
+      set.setAttribute("class", "fa fa-plus")
+    } else if (showElement.style.display === "none") {
+      showElement.style.display = "block";
+      let set = document.getElementById("cros4")
+      set.setAttribute("class", "fa fa-times")
+    } else {
+      showElement.style.display = "none";
+    }
+  }
+   function show5(){
+    var showElement = document.getElementById("show5");
+  
+    if (showElement.style.display === "block") {
+      showElement.style.display = "none";
+      let set = document.getElementById("cros5")
+      set.setAttribute("class", "fa fa-plus")
+    } else if (showElement.style.display === "none") {
+      showElement.style.display = "block";
+      let set = document.getElementById("cros5")
+      set.setAttribute("class", "fa fa-times")
+    } else {
+      showElement.style.display = "none";
+    }
+  }
+  
+   function show6(){
+    var showElement = document.getElementById("show6");
+  
+    if (showElement.style.display === "block") {
+      showElement.style.display = "none";
+      let set = document.getElementById("cros6")
+      set.setAttribute("class", "fa fa-plus")
+    } else if (showElement.style.display === "none") {
+      showElement.style.display = "block";
+      let set = document.getElementById("cros6")
+      set.setAttribute("class", "fa fa-times")
+    } else {
+      showElement.style.display = "none";
+    }
+  }
+  
+  function leave() {
+    
+    var showElement = document.getElementById("show6");
+    showElement.style.display ="none";
+  }
+   
